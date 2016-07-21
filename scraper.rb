1d932b840d5fe437fa8c2cb64cd7ab26ab1a9828
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-07-21 16:30:58 +1000

    Fixes #2, now we have links to individual planning applications

--- scraper.rb
+++ scraper.rb
@@ -16,13 +16,16 @@ def save_table_data(table_rows, url)
 
     #The tables come with a header tr[0] and two tr at bottom tr[21], tr[22]. This function wants to access the table data only, so we skip these header and footer rows
     if index == 0 or index == table_tr_number-1 or index == table_tr_number-2
-      next 
+      next
     end
 
+    council_reference = tr.search("td")[0].inner_text
+    link_to_decision = "https://eproperty.wyndham.vic.gov.au/ePropertyPROD/P1/eTrack/eTrackApplicationDetails.aspx?r=P1.WEBGUEST&f=%24P1.ETR.APPDET.VIW&ApplicationId=#{council_reference}"
+
     record = {
-      "info_url" => url,
-      "comment_url" => url,
-      "council_reference" => tr.search("td")[0].inner_text, 
+      "info_url" => link_to_decision,
+      "comment_url" => link_to_decision,
+      "council_reference" => council_reference, 
       "date_received" => tr.search("td")[1].inner_text,
       "description" => tr.search("td")[2].inner_text,
       "address" => tr.search("td")[3].inner_text,
@@ -30,12 +33,12 @@ def save_table_data(table_rows, url)
       "decision" => tr.search("td")[5].inner_text,
       "date_scraped" => Date.today.to_s
     }
-    
+
     if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
       ScraperWiki.save_sqlite(['council_reference'], record)
-      #puts record
-    #else
-      #puts "Skipping already saved record " + record['council_reference']
+      puts "saving " + record['council_reference']
+    else
+      puts "Skipping already saved record " + record['council_reference']
     end
 
   end
